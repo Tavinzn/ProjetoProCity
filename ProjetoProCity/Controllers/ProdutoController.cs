76cfa4a2b5756fb36d4ecfcfbe506cbd5fe2a53e
tavinzn <otaviosantosandrigo08@gmail.com>
2025-05-10 13:48:48 -0300

    Configurando a controllerProduto

--- ProjetoProCity/Controllers/ProdutoController.cs
+++ ProjetoProCity/Controllers/ProdutoController.cs
@@ -1,6 +1,77 @@
-﻿namespace ProjetoProCity.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoProCity.Models;
+using ProjetoProCity.Repositorio;
+
+namespace ProjetoProCity.Controllers
 {
-    public class ProdutoController
+    public class ProdutoController : Controller
     {
+        private readonly ProdutoRepositorio _produtoRepositorio;
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_produtoRepositorio.TodosProdutos());
+        }
+
+        public IActionResult CadastrarProduto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarProduto(Produto produto)
+        {
+            _produtoRepositorio.Cadastrar(produto);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult EditarProduto(int id)
+        {
+            var produto = _produtoRepositorio.ObterProduto(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditarProduto(int id, [Bind("Id, Nome, Descricao, Preco, Quantidade")] Produto produto)
+        {
+            if (id != produto.Id)
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (_produtoRepositorio.Atualizar(produto))
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Ocorreu um erro ao tentar atualizar o produto");
+                    return View(produto);
+                }
+            }
+            return View(produto);
+        }
+
+        public IActionResult ExcluirProduto(int id)
+        {
+            _produtoRepositorio.Excluir(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
